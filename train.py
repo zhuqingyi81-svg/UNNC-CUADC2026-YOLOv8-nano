04f22b9ea5b1ad1c701b86556f3f1afca5d7474d
Tachyon. <zhuqingyi81@gmail.com>
2026-07-14 04:28:14 -0700

    Add files via upload

---
+++ train.py
@@ -0,0 +1,7 @@
+
+from ultralytics import YOLO
+
+model = YOLO('yolov8n.pt')
+
+# 把这里的路径，换成你刚刚右键复制的完整路径！注意保留两边的引号！
+model.train(data=r'C:\Users\Mercury\OneDrive\Desktop\UNNC CUADC2026\mydata\YOLODataset\dataset.yaml', epochs=50, imgsz=640)
