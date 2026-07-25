d0cff275288fafeb451345c91faba595ebe5d0e9
Tachyon. <zhuqingyi81@gmail.com>
2026-07-24 23:59:09 -0700

    Update training parameters in train.py
    Increased training epochs to 100, changed image size to 1280, and added patience and batch size parameters.

--- train.py
+++ train.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 
 # 把这里的路径，换成你刚刚右键复制的完整路径！注意保留两边的引号！
-model.train(data=r'C:\Users\Mercury\OneDrive\Desktop\UNNC CUADC2026\mydata\YOLODataset\dataset.yaml', epochs=50, imgsz=640)
+model.train(data=r'C:\Users\Mercury\OneDrive\Desktop\UNNC CUADC2026\mydata\YOLODataset\dataset.yaml', epochs=100, imgsz=1280，patience=15, batch=16)
